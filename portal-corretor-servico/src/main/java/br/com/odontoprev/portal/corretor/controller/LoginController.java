457b3e40683c0f755189e1df884b208d55c0d371
Leandro Jacomelli <leandro.jacomelli@gmail.com>
2018-02-25 20:32:31 -0300

    ajuste do response code

--- portal-corretor-servico/src/main/java/br/com/odontoprev/portal/corretor/controller/LoginController.java
+++ portal-corretor-servico/src/main/java/br/com/odontoprev/portal/corretor/controller/LoginController.java
@@ -4,6 +4,8 @@ import br.com.odontoprev.portal.corretor.dto.Login;
 import br.com.odontoprev.portal.corretor.dto.LoginResponse;
 import br.com.odontoprev.portal.corretor.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,8 +20,10 @@ public class LoginController {
     private LoginService loginService;
 
     @RequestMapping(value = "/login", method = {RequestMethod.POST})
-    public LoginResponse login(@RequestBody @Valid Login login) {
-        return loginService.login(login);
+    public ResponseEntity<LoginResponse> login(@RequestBody @Valid Login login) {
+        LoginResponse res = loginService.login(login);
+
+        return res.getSucesso() ? ResponseEntity.ok(res) : ResponseEntity.status(HttpStatus.FORBIDDEN).body(res);
     }
 
 
