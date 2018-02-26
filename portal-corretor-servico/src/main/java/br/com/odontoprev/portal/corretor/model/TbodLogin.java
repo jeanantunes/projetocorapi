d53b727b54d9689f635aeabd2bb6870d3af0456e
Jaqueline Alves da Silva <feodpv01004@odontoprev.com.br>
2018-02-26 18:36:36 -0300

    Ajuste servico post forca venda

--- portal-corretor-servico/src/main/java/br/com/odontoprev/portal/corretor/model/TbodLogin.java
+++ portal-corretor-servico/src/main/java/br/com/odontoprev/portal/corretor/model/TbodLogin.java
@@ -1,41 +1,51 @@
 package br.com.odontoprev.portal.corretor.model;
 
 import java.io.Serializable;
-import javax.persistence.*;
-import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 /**
  * The persistent class for the TBOD_LOGIN database table.
  * 
  */
 @Entity
-@Table(name="TBOD_LOGIN")
-@NamedQuery(name="TbodLogin.findAll", query="SELECT t FROM TbodLogin t")
+@Table(name = "TBOD_LOGIN")
+@NamedQuery(name = "TbodLogin.findAll", query = "SELECT t FROM TbodLogin t")
 public class TbodLogin implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@Column(name="CD_LOGIN")
+	@SequenceGenerator(name = "SEQ_TBOD_LOGIN", sequenceName = "SEQ_TBOD_LOGIN",  allocationSize = 1, initialValue = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_TBOD_LOGIN")
+	@Column(name = "CD_LOGIN")
 	private long cdLogin;
 
-	@Column(name="CD_TIPO_LOGIN")
-	private BigDecimal cdTipoLogin;
+	@Column(name = "CD_TIPO_LOGIN")
+	private Long cdTipoLogin;
 
-	@Column(name="FOTO_PERFIL_B64")
+	@Column(name = "FOTO_PERFIL_B64")
 	private String fotoPerfilB64;
 
-	@Column(name="SENHA")
+	@Column(name = "SENHA")
 	private String senha;
 
-	//bi-directional one-to-one association to TbodCorretora
+	// bi-directional one-to-one association to TbodCorretora
 	@ManyToOne
-	@JoinColumn(name="CD_FORCA_VENDA_CORRETORA", insertable=false, updatable=false)
+	@JoinColumn(name = "CD_FORCA_VENDA_CORRETORA", insertable = false, updatable = false)
 	private TbodCorretora tbodCorretora;
 
-	//bi-directional many-to-one association to TbodForcaVenda
+	// bi-directional many-to-one association to TbodForcaVenda
 	@ManyToOne
-	@JoinColumn(name="CD_FORCA_VENDA_CORRETORA", insertable=false, updatable=false)
+	@JoinColumn(name = "CD_FORCA_VENDA_CORRETORA", insertable = false, updatable = false)
 	private TbodForcaVenda tbodForcaVenda;
 
 	public TbodLogin() {
@@ -49,11 +59,11 @@ public class TbodLogin implements Serializable {
 		this.cdLogin = cdLogin;
 	}
 
-	public BigDecimal getCdTipoLogin() {
-		return this.cdTipoLogin;
+	public Long getCdTipoLogin() {
+		return cdTipoLogin;
 	}
 
-	public void setCdTipoLogin(BigDecimal cdTipoLogin) {
+	public void setCdTipoLogin(Long cdTipoLogin) {
 		this.cdTipoLogin = cdTipoLogin;
 	}
 
