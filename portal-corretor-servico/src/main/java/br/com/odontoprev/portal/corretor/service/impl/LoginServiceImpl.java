79972cdbb7ca159c45b0e1cfa548e0c5b383e50e
Leandro Jacomelli <leandro.jacomelli@gmail.com>
2018-02-26 00:36:40 -0300

    integracao com  DCSS no cadastramento de forca de venda

--- portal-corretor-servico/src/main/java/br/com/odontoprev/portal/corretor/service/impl/LoginServiceImpl.java
+++ portal-corretor-servico/src/main/java/br/com/odontoprev/portal/corretor/service/impl/LoginServiceImpl.java
@@ -9,6 +9,7 @@ import br.com.odontoprev.portal.corretor.service.LoginService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -25,6 +26,9 @@ public class LoginServiceImpl implements LoginService {
     @Autowired
     LoginDAO loginDAO;
 
+    @Value("${DCSS_URL}")
+    private String dcssUrl;
+
 
     @Override
     public LoginResponse login(Login login) {
@@ -38,7 +42,7 @@ public class LoginServiceImpl implements LoginService {
                 loginMap.put("senha", login.getSenha());
                 try {
 
-                    ResponseEntity<LoginRetorno> loginRetorno = restTemplate.postForEntity(("https://api-it1.odontoprev.com.br:8243/dcss/login/1.0/?"), loginMap, LoginRetorno.class);
+                    ResponseEntity<LoginRetorno> loginRetorno = restTemplate.postForEntity((dcssUrl+"/login/1.0/?"), loginMap, LoginRetorno.class);
                     return new LoginResponse(login.getUsuario(), true, Long.parseLong(loginRetorno.getBody().getCodigo()));
                 } catch (Exception e) {
                     return new LoginResponse(null, false, null);
