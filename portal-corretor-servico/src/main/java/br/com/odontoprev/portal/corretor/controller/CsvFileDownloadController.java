8fa399503092f3cbd949797e6a6b0f31572bba8f
Jaqueline Alves <jaqueline.alves@outlook.com>
2018-03-28 09:07:52 -0300

    refactory csv

--- portal-corretor-servico/src/main/java/br/com/odontoprev/portal/corretor/controller/CsvFileDownloadController.java
+++ portal-corretor-servico/src/main/java/br/com/odontoprev/portal/corretor/controller/CsvFileDownloadController.java
@@ -1,11 +1,9 @@
 package br.com.odontoprev.portal.corretor.controller;
 
 
-import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
-import javax.websocket.server.PathParam;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -14,12 +12,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.supercsv.io.CsvBeanWriter;
-import org.supercsv.io.ICsvBeanWriter;
-import org.supercsv.prefs.CsvPreference;
 
 import br.com.odontoprev.portal.corretor.dto.RelatorioGestaoVenda;
 import br.com.odontoprev.portal.corretor.service.RelatorioGestaoVendaService;
+import br.com.odontoprev.portal.corretor.util.CsvUtil;
 
 @RestController
 public class CsvFileDownloadController {
@@ -34,57 +30,14 @@ public class CsvFileDownloadController {
 		
 		log.info("downloadCSV");
 		
-		try {
-			
-			List<RelatorioGestaoVenda> gestaoVendas = relatorioGestaoVendaService.findVendasByCorretora(cnpj);
-			
-			String csvFileName = "relatorio-gestao-vendas.csv";
-			
-			response.setContentType("text/csv");
-			
-	        String headerKey = "Content-Disposition";
-	        String headerValue = String.format("attachment; filename=\"%s\"", csvFileName);
-	        response.setHeader(headerKey, headerValue);
-	        
-			ICsvBeanWriter csvWriter = new CsvBeanWriter(response.getWriter(),
-			        CsvPreference.STANDARD_PREFERENCE);
-			
-			String[] header = {
-					"DataVenda", 
-					"Nome", 
-					"Cpf", 
-					"DataNascimento",
-					"NomeMae",
-					"Celular",
-					"Email",
-					"Cep",
-					"Logradouro",
-					"Numero",
-					"Bairro",
-					"Complemento",
-					"Cidade",
-					"UF",
-					"NomePlano",
-					"TipoPlano",
-					"ValorMensal",
-					"ValorAnual"
-				};
-			
-			csvWriter.writeHeader(header);
-			
-			for (RelatorioGestaoVenda venda : gestaoVendas) {
-				csvWriter.write(venda, header);
-		    }
-			
-		    csvWriter.close();
-
-		} 
-		catch (IOException e) {
-			log.error("Erro ao tentar realizar download Relatorio Gestao Vendas. Detalhe: [" + e.getMessage() + "]");
-		} catch (Exception e) {
-			log.error("Erro ao tentar realizar download Relatorio Gestao Vendas. Detalhe: [" + e.getMessage() + "]");
-		} 
+		List<RelatorioGestaoVenda> gestaoVendas = relatorioGestaoVendaService.findVendasByCorretora(cnpj);
+		
+		if(gestaoVendas != null && !gestaoVendas.isEmpty()) {
+			CsvUtil.gerarCsv(response, gestaoVendas);
+		}
 		
 	}
+
+	
 
 }
