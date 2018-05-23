3916a965598a7145de921012740f319dd721fc0e
feodpv00940 <victor.rodrigues@parceiros.odontoprev.com.br>
2018-05-23 12:33:35 -0300

    Criação do DAO NotificacaoDAO

---
+++ portal-corretor-servico/src/main/java/br/com/odontoprev/portal/corretor/dao/NotificacaoDAO.java
@@ -0,0 +1,22 @@
+package br.com.odontoprev.portal.corretor.dao;
+
+import br.com.odontoprev.portal.corretor.model.TbodForcaVenda;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NotificacaoDAO extends CrudRepository<TbodForcaVenda, String> {
+	//public interface NotificacaoDAO extends CrudRepository<TbodNotificationTemplate, String> {
+
+	@Query(value=" SELECT NOTIFICATION.* "
+			+ " FROM TBOD_NOTIFICATION_TEMPLATE NOTIFICATION "
+			+ " WHERE NOTIFICATION.TIPO = :tipo "
+			, nativeQuery=true)
+
+	public String findbyTipo(@Param("tipo") String tipo);
+	
+}
