48745bc3b1e93140998ea094f4efcaf8fd27754a
ROBERTO <roberto.msilva@odontoprev.com.br>
2018-06-04 12:13:06 -0300

    interceptor. (work in progress) inativo. apenas versionar. (esert)

---
+++ portal-corretor-servico/src/main/java/br/com/odontoprev/portal/corretor/interceptor/Auditor.java
@@ -0,0 +1,12 @@
+package br.com.odontoprev.portal.corretor.interceptor;
+
+import java.security.Principal;
+
+public class Auditor {
+
+	public void audit(String requestURI, Principal userPrincipal, String body) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
