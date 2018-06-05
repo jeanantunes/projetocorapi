c76f3b94f90ba8d2422629bcfe64bcc79abad6af
ROBERTO <roberto.msilva@odontoprev.com.br>
2018-06-05 16:03:54 -0300

    desligando dependencias do interceptor na branch sprint6 (esert)

--- portal-corretor-servico/src/main/java/br/com/odontoprev/portal/corretor/interceptor/Auditor.java
+++ portal-corretor-servico/src/main/java/br/com/odontoprev/portal/corretor/interceptor/Auditor.java
@@ -1,20 +1,20 @@
-package br.com.odontoprev.portal.corretor.interceptor;
-
-import java.security.Principal;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-public class Auditor {
-	private static final Log log = LogFactory.getLog(Auditor.class);
-
-
-	public void audit(String requestURI, Principal userPrincipal, String body) {
-		// TODO Auto-generated method stub
-		log.info("requestURI:[" + requestURI + "]"); //201806041509 - esert
-		log.info("userPrincipal.getName():[" + userPrincipal.getName() + "]"); //201806041509 - esert
-		log.info("body:[" + body + "]"); //201806041519 - esert
-		
-	}
-
-}
+//package br.com.odontoprev.portal.corretor.interceptor;
+//
+//import java.security.Principal;
+//
+//import org.apache.commons.logging.Log;
+//import org.apache.commons.logging.LogFactory;
+//
+//public class Auditor {
+//	private static final Log log = LogFactory.getLog(Auditor.class);
+//
+//
+//	public void audit(String requestURI, Principal userPrincipal, String body) {
+//		// TODO Auto-generated method stub
+//		log.info("requestURI:[" + requestURI + "]"); //201806041509 - esert
+//		log.info("userPrincipal.getName():[" + userPrincipal.getName() + "]"); //201806041509 - esert
+//		log.info("body:[" + body + "]"); //201806041519 - esert
+//		
+//	}
+//
+//}
--- portal-corretor-servico/src/main/java/br/com/odontoprev/portal/corretor/interceptor/LoggerInterceptor.java
+++ portal-corretor-servico/src/main/java/br/com/odontoprev/portal/corretor/interceptor/LoggerInterceptor.java
@@ -1,89 +1,91 @@
-package br.com.odontoprev.portal.corretor.interceptor;
-
-import java.util.Enumeration;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
-import br.com.odontoprev.portal.corretor.interceptor.LoggerFilter.ResettableStreamHttpServletRequest;
-
-public class LoggerInterceptor extends HandlerInterceptorAdapter {
-
-	private static final Log log = LogFactory.getLog(LoggerInterceptor.class);
-
-	@Override
-	public boolean preHandle(
-			HttpServletRequest request, 
-			HttpServletResponse response, 
-			Object handler
-			)
-			throws Exception {
-
-		log.info("[preHandle][" + request + "]" 
-				+ "[" + request.getMethod() + "]" 
-				+ request.getRequestURI()
-				+ getParameters(request)
-				);
-		
-		//ResettableStreamHttpServletRequest wrappedRequest = new ResettableStreamHttpServletRequest((HttpServletRequest) request); //201806041520 - esert
-		////// wrappedRequest.getInputStream().read();
-		//String body = IOUtils.toString(wrappedRequest.getReader());
-
-		//log.info("[preHandle]; body:[" + body + "]");
-		
-		return true;
-	}
-
-	private String getParameters(HttpServletRequest request) {
-		
-		StringBuffer posted = new StringBuffer();
-		Enumeration<?> e = request.getParameterNames();
-		
-		if (e != null) {
-			posted.append("?");
-		}
-		while (e.hasMoreElements()) {
-			if (posted.length() > 1) {
-				posted.append("&");
-			}
-			String curr = (String) e.nextElement();
-			posted.append(curr + "=");
-			if (curr.contains("password") || curr.contains("pass") || curr.contains("pwd")) {
-				posted.append("*****");
-			} else {
-				posted.append(request.getParameter(curr));
-			}
-		}
-		String ip = request.getHeader("X-FORWARDED-FOR");
-		String ipAddr = (ip == null) ? getRemoteAddr(request) : ip;
-		if (ipAddr != null && !ipAddr.equals("")) {
-			posted.append("&_psip=" + ipAddr);
-		}
-		return posted.toString();
-	}
-
-	private String getRemoteAddr(HttpServletRequest request) {
-		String ipFromHeader = request.getHeader("X-FORWARDED-FOR");
-		if (ipFromHeader != null && ipFromHeader.length() > 0) {
-			log.debug("ip from proxy - X-FORWARDED-FOR : " + ipFromHeader);
-			return ipFromHeader;
-		}
-		return request.getRemoteAddr();
-	}
-	
-	@Override
-	public void postHandle(
-	  HttpServletRequest request, 
-	  HttpServletResponse response,
-	  Object handler, 
-	  ModelAndView modelAndView) throws Exception {
-	     
-	    log.info("[postHandle][" + request + "]");
-	}
-}
+//package br.com.odontoprev.portal.corretor.interceptor;
+//
+//import java.util.Enumeration;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//import org.apache.commons.io.IOUtils;
+//import org.apache.commons.logging.Log;
+//import org.apache.commons.logging.LogFactory;
+//import org.springframework.web.servlet.ModelAndView;
+//import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+//
+//import br.com.odontoprev.portal.corretor.interceptor.LoggerFilter.ResettableStreamHttpServletRequest;
+//
+//public class LoggerInterceptor extends HandlerInterceptorAdapter {
+//
+//	private static final Log log = LogFactory.getLog(LoggerInterceptor.class);
+//
+//	@Override
+//	public boolean preHandle(
+//			HttpServletRequest request, 
+//			HttpServletResponse response, 
+//			Object handler
+//			)
+//			throws Exception {
+//
+//		log.info("[preHandle][" + request + "]" 
+//				+ "[" + request.getMethod() + "]" 
+//				+ request.getRequestURI()
+//				+ getParameters(request)
+//				);
+//		
+//		String body = null;
+//		//ResettableStreamHttpServletRequest wrappedRequest = new ResettableStreamHttpServletRequest((HttpServletRequest) request); //201806041520 - esert
+//		////// wrappedRequest.getInputStream().read();
+//		//body = wrappedRequest.getInputStream().toString();
+//		//body = IOUtils.toString(wrappedRequest.getReader());
+//
+//		log.info("[preHandle]; body:[" + body + "]");
+//		
+//		return true;
+//	}
+//
+//	private String getParameters(HttpServletRequest request) {
+//		
+//		StringBuffer posted = new StringBuffer();
+//		Enumeration<?> e = request.getParameterNames();
+//		
+//		if (e != null) {
+//			posted.append("?");
+//		}
+//		while (e.hasMoreElements()) {
+//			if (posted.length() > 1) {
+//				posted.append("&");
+//			}
+//			String curr = (String) e.nextElement();
+//			posted.append(curr + "=");
+//			if (curr.contains("password") || curr.contains("pass") || curr.contains("pwd")) {
+//				posted.append("*****");
+//			} else {
+//				posted.append(request.getParameter(curr));
+//			}
+//		}
+//		String ip = request.getHeader("X-FORWARDED-FOR");
+//6		String ipAddr = (ip == null) ? getRemoteAddr(request) : ip;
+//		if (ipAddr != null && !ipAddr.equals("")) {
+//			posted.append("&_psip=" + ipAddr);
+//		}
+//		return posted.toString();
+//	}
+//
+//	private String getRemoteAddr(HttpServletRequest request) {
+//		String ipFromHeader = request.getHeader("X-FORWARDED-FOR");
+//		if (ipFromHeader != null && ipFromHeader.length() > 0) {
+//			log.debug("ip from proxy - X-FORWARDED-FOR : " + ipFromHeader);
+//			return ipFromHeader;
+//		}
+//		return request.getRemoteAddr();
+//	}
+//	
+//	@Override
+//	public void postHandle(
+//	  HttpServletRequest request, 
+//	  HttpServletResponse response,
+//	  Object handler, 
+//	  ModelAndView modelAndView) throws Exception {
+//	     
+//	    log.info("[postHandle][" + request + "]");
+//	}
+//}
