e26ff1819bf1925dde5b9cbafc696c55f37107f8
ROBERTO <roberto.msilva@odontoprev.com.br>
2018-06-11 22:13:11 -0300

    rename seq e table log email boas vindas pme erro nome maior 30 (esert)

--- portal-corretor-servico/src/main/java/br/com/odontoprev/portal/corretor/model/TbodLogEmailBoasVindasPME.java
+++ portal-corretor-servico/src/main/java/br/com/odontoprev/portal/corretor/model/TbodLogEmailBoasVindasPME.java
@@ -12,18 +12,24 @@ import javax.persistence.NamedQuery;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
+/*
+//2018061112117 - esert - nome reduzido de SEQ_TBOD_LOG_EMAIL_BOASVINDASPME(tam 32) para SEQ_TBOD_LOG_EMAIL_BVPME(tam 24) para nao passar de 30 trinta no nome dos objetos
+//evitar Erro "ORA-00972: identifier is too long" (ORA-00972: identificador é muito longo)
+//Ao tentar usar um objeto do banco de dados da Oracle (por exemplo, coluna ou tabela) com um nome mais longo que trinta (30) caracteres, o erro a seguir pode ocorrer:
+*/
+
 //201805221239 - esert - COR-225 - Serviço - LOG Envio e-mail de Boas Vindas PME
 //Sugestão de nome para a tabela: TBOD_LOG_EMAIL_BOASVINDASPME 
 @Entity
-@Table(name = "TBOD_LOG_EMAIL_BOASVINDASPME")
+@Table(name = "TBOD_LOG_EMAIL_BVPME")
 @NamedQuery(name = "TbodLogEmailBoasVindasPME.findAll", query = "SELECT t FROM TbodLogEmailBoasVindasPME t")
 public class TbodLogEmailBoasVindasPME implements Serializable {
 
 	private static final long serialVersionUID = -5838590203922555875L;
 
 	@Id
-	@SequenceGenerator(name = "SEQ_TBOD_LOG_EMAIL_BOASVINDASPME", sequenceName = "SEQ_TBOD_LOG_EMAIL_BOASVINDASPME", allocationSize = 1, initialValue = 1)
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_TBOD_LOG_EMAIL_BOASVINDASPME")
+	@SequenceGenerator(name = "SEQ_TBOD_LOG_EMAIL_BVPME", sequenceName = "SEQ_TBOD_LOG_EMAIL_BVPME", allocationSize = 1, initialValue = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_TBOD_LOG_EMAIL_BVPME")
 	@Column(name = "ID")
 	private Long id;
 
