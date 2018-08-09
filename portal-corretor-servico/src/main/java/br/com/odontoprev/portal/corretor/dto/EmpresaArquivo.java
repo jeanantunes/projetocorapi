cffa84e57c10e90bb527b3f8cc39a8e646c46a72
Yago Almeida Santos <yalmeida@vectoritcgroup.com>
2018-08-09 17:06:54 -0300

    [COR-545] - Adicionado mais testes e criado equals e hashCode da classe

--- portal-corretor-servico/src/main/java/br/com/odontoprev/portal/corretor/dto/EmpresaArquivo.java
+++ portal-corretor-servico/src/main/java/br/com/odontoprev/portal/corretor/dto/EmpresaArquivo.java
@@ -2,12 +2,27 @@ package br.com.odontoprev.portal.corretor.dto;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Objects;
 
 public class EmpresaArquivo implements Serializable {
 
     private static final long serialVersionUID = 3103017260662982091L; //TODO: Alterar serialVersionUID
 
-    private Long cdEmpresaTest;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof EmpresaArquivo)) return false;
+		EmpresaArquivo that = (EmpresaArquivo) o;
+		return Objects.equals(getListCdEmpresa(), that.getListCdEmpresa());
+	}
+
+	@Override
+	public int hashCode() {
+
+		return Objects.hash(getListCdEmpresa());
+	}
+
+	private Long cdEmpresaTest;
     
     private List<Long> listCdEmpresa;
 
--- portal-corretor-servico/src/main/java/br/com/odontoprev/portal/corretor/dto/EmpresaArquivoResponse.java
+++ portal-corretor-servico/src/main/java/br/com/odontoprev/portal/corretor/dto/EmpresaArquivoResponse.java
@@ -2,8 +2,23 @@ package br.com.odontoprev.portal.corretor.dto;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Objects;
 
 public class EmpresaArquivoResponse implements Serializable {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof EmpresaArquivoResponse)) return false;
+        EmpresaArquivoResponse that = (EmpresaArquivoResponse) o;
+        return Objects.equals(getEmpresas(), that.getEmpresas());
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(getEmpresas());
+    }
 
     private static final long serialVersionUID = 1L; //TODO: Alterar serialVersionUID
 
