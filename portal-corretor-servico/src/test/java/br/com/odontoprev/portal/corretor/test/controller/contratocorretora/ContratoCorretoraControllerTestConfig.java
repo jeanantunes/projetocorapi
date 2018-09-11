25d449220e19d7a9cf9bf9a880ae859fe57cb2b7
ROBERTO <roberto.msilva@odontoprev.com.br>
2018-09-11 19:11:43 -0300

    ajustes ContratoCorretoraControllerTest (esert)

--- portal-corretor-servico/src/test/java/br/com/odontoprev/portal/corretor/test/controller/contratocorretora/ContratoCorretoraControllerTestConfig.java
+++ portal-corretor-servico/src/test/java/br/com/odontoprev/portal/corretor/test/controller/contratocorretora/ContratoCorretoraControllerTestConfig.java
@@ -1,18 +1,25 @@
 package br.com.odontoprev.portal.corretor.test.controller.contratocorretora;
 
+import org.mockito.Mockito;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 import br.com.odontoprev.portal.corretor.controller.ContratoCorretoraController;
 import br.com.odontoprev.portal.corretor.service.ContratoCorretoraService;
-import org.mockito.Mockito;
-import org.springframework.context.annotation.*;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 //Classe responsável por configurar objetos que queremos mockar
 @Configuration
+
 @ComponentScan(
 		value = "br.com.odontoprev.portal.corretor.controller",
 	    useDefaultFilters = false,
 	    includeFilters = {
-	        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = ContratoCorretoraController.class)
+	        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = ContratoCorretoraController.class),
 	    })
 
 @EnableWebMvc
