81eebcef36bad88e9319cac064b1d971fb8ed138
ROBERTO <roberto.msilva@odontoprev.com.br>
2018-09-11 20:32:59 -0300

    DeviceTokenController.java org import (esert)

--- portal-corretor-servico/src/main/java/br/com/odontoprev/portal/corretor/controller/DeviceTokenController.java
+++ portal-corretor-servico/src/main/java/br/com/odontoprev/portal/corretor/controller/DeviceTokenController.java
@@ -9,11 +9,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import br.com.odontoprev.portal.corretor.dto.DeviceToken;
 import br.com.odontoprev.portal.corretor.service.DeviceTokenService;
-import br.com.odontoprev.portal.corretor.service.impl.ApiManagerTokenServiceImpl;
 
 @Controller
 public class DeviceTokenController implements Serializable {
