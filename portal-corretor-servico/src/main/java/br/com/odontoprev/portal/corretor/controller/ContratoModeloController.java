1fbb8b027bcc35b4fd2bdf27c58616947e01cfde
ROBERTO <roberto.msilva@odontoprev.com.br>
2018-09-12 12:15:30 -0300

    COR-759 cria GET/contratomodelo/id/tipo/cdTipo/arquivo validacao html (esert)

--- portal-corretor-servico/src/main/java/br/com/odontoprev/portal/corretor/controller/ContratoModeloController.java
+++ portal-corretor-servico/src/main/java/br/com/odontoprev/portal/corretor/controller/ContratoModeloController.java
@@ -2,18 +2,24 @@ package br.com.odontoprev.portal.corretor.controller;
 
 import java.io.File;
 import java.text.ParseException;
+import java.util.Optional;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.odontoprev.portal.corretor.dto.ContratoCorretoraPreenchido;
 import br.com.odontoprev.portal.corretor.dto.ContratoModelo;
+import br.com.odontoprev.portal.corretor.service.ContratoCorretoraService;
 import br.com.odontoprev.portal.corretor.service.ContratoModeloService;
 
 @RestController
@@ -23,6 +29,9 @@ public class ContratoModeloController {
 
 	@Autowired
 	private ContratoModeloService contratoModeloService;
+
+	@Autowired
+	private ContratoCorretoraService contratoCorretoraService;
 
 	//201809111030 - esert - COR-760 - Serviço - cria POST/contratomodelo
 	@RequestMapping(value = "/contratomodelo/carregar/arquivo", method = { RequestMethod.POST})
@@ -79,6 +88,36 @@ public class ContratoModeloController {
 		
 		log.info("carregarMaterialDivulgacaoArquivo - fim");	
 		return ResponseEntity.ok(responseObject);
+	}
+
+	//201809112252 - esert - COR-709 - Serviço - Novo serviço GET/contratocorretora/cdCorr/tipo/cdTipo/arquivo?susep=cdSusep
+	//201809112315 - esert - COR-759 - Serviço - cria GET/contratomodelo/id/tipo/cdTipo/arquivo para rapida validacao modelo html
+	@RequestMapping(value = "/contratomodelo/{cdCorretora}/tipo/{cdContratoModelo}/arquivo", method = {RequestMethod.GET})
+	public ResponseEntity<byte[]> getContratoModeloPreenchidoByteArray(@PathVariable Long cdCorretora, @PathVariable Long cdContratoModelo, @RequestParam("susep") Optional<String> cdSusep) throws ParseException {
+		log.info("getContratoPreenchidoByteArray - ini");
+		try {
+			ContratoCorretoraPreenchido contratoCorretoraPreenchido = contratoCorretoraService.getContratoPreenchido(cdCorretora, cdContratoModelo, cdSusep.orElse(null));
+			if(contratoCorretoraPreenchido==null) {
+				return ResponseEntity.status(HttpStatus.NO_CONTENT).build(); //201808241726 - esert
+			}
+		    //byte[] arquivoHTML = Base64.getDecoder().decode(contratoCorretoraPreenchido.getContratoPreenchido());
+		    byte[] arquivoHTML = contratoCorretoraPreenchido.getContratoPreenchido().getBytes();
+		    String[] tipoConteudo = contratoCorretoraPreenchido.getTipoConteudo().split("/");
+		    String type = tipoConteudo[0];
+		    String subType = tipoConteudo[1];
+			log.info("getArquivoByteArray - fim");	
+		    return ResponseEntity
+		    		.ok()
+		    		.contentType(new MediaType(type, subType))
+		    		.header(
+		    				"Content-Disposition", 
+		    				String.format("attachment; filename=%s", contratoCorretoraPreenchido.getNomeArquivo())
+		    				)
+		    		.body(arquivoHTML);
+		} catch (Exception e) {
+			log.info("getContratoPreenchidoByteArray - erro");	
+		    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build(); //201808241726 - esert
+		}
 	}
 
 }
