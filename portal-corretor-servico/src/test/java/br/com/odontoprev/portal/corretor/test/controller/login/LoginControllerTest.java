50f68a6448affb72101a8907a5a70c05772268c3
Jean Antunes <jean.antunes@gmail.com>
2018-09-18 18:38:39 -0300

    COR-814 LoginControllerTest String to Constates

--- portal-corretor-servico/src/test/java/br/com/odontoprev/portal/corretor/test/controller/login/LoginControllerTest.java
+++ portal-corretor-servico/src/test/java/br/com/odontoprev/portal/corretor/test/controller/login/LoginControllerTest.java
@@ -3,6 +3,7 @@ package br.com.odontoprev.portal.corretor.test.controller.login;
 import br.com.odontoprev.portal.corretor.dto.Login;
 import br.com.odontoprev.portal.corretor.dto.LoginResponse;
 import br.com.odontoprev.portal.corretor.service.LoginService;
+import br.com.odontoprev.portal.corretor.util.Constantes;
 import com.google.gson.Gson;
 import org.junit.Before;
 import org.junit.Test;
@@ -55,7 +56,7 @@ public class LoginControllerTest {
         login.setSenha("10203040");
         login.setUsuario("64154543000146");
         login.setTemBloqueio(false);
-        login.setCodigoTipoBloqueio(0L); // 1
+        login.setCodigoTipoBloqueio(Constantes.TIPO_BLOQUEIO_SEM_BLOQUEIO); // Constantes.TIPO_BLOQUEIO_CORRETAGEM_INTERMEDIACAO
         login.setDescricaoTipoBloqueio("SEM BLOQUEIO"); //PENDENTE ACEITAR CONTRATO DE CORRETAGEM OU INTERMEDIAÇÃO
 
         LoginResponse loginResponse = new LoginResponse();
@@ -94,7 +95,7 @@ public class LoginControllerTest {
         login.setSenha("10203040");
         login.setUsuario("12345678909");
         login.setTemBloqueio(false);
-        login.setCodigoTipoBloqueio(0L); // 1
+        login.setCodigoTipoBloqueio(Constantes.TIPO_BLOQUEIO_SEM_BLOQUEIO); // Constantes.TIPO_BLOQUEIO_CORRETAGEM_INTERMEDIACAO
         login.setDescricaoTipoBloqueio("SEM BLOQUEIO"); //PENDENTE ACEITAR CONTRATO DE CORRETAGEM OU INTERMEDIAÇÃO
 
         LoginResponse loginResponse = new LoginResponse();
