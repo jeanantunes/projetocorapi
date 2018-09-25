357d8219e0f82930e90b3d8826f22dabe7280a78
ROBERTO <roberto.msilva@odontoprev.com.br>
2018-09-25 20:06:36 -0300

    COR-820 Serviço Criar POST /empresa-emailaceite (esert)

--- portal-corretor-servico/src/main/java/br/com/odontoprev/portal/corretor/dto/EmpresaEmailAceite.java
+++ portal-corretor-servico/src/main/java/br/com/odontoprev/portal/corretor/dto/EmpresaEmailAceite.java
@@ -33,7 +33,50 @@ public class EmpresaEmailAceite implements Serializable { //201805111538 - esert
 	
 	@Override
 	public String toString() {
-		return "EmpresaEmailAceite [cdEmpresa=" + cdEmpresa + ", cdVenda=" + cdVenda + ", email=" + email + "]";
+		return "EmpresaEmailAceite [" 
+				+ "cdEmpresa=" + cdEmpresa 
+				+ ", cdVenda=" + cdVenda 
+				+ ", email=" + email 
+				+ "]";
 	}
 	
+	//20189251948 - esert - COR-820 : Serviço - Criar POST /empresa-emailaceite
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cdEmpresa == null) ? 0 : cdEmpresa.hashCode());
+		result = prime * result + ((cdVenda == null) ? 0 : cdVenda.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		return result;
+	}
+	
+	//20189251948 - esert - COR-820 : Serviço - Criar POST /empresa-emailaceite
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EmpresaEmailAceite other = (EmpresaEmailAceite) obj;
+		if (cdEmpresa == null) {
+			if (other.cdEmpresa != null)
+				return false;
+		} else if (!cdEmpresa.equals(other.cdEmpresa))
+			return false;
+		if (cdVenda == null) {
+			if (other.cdVenda != null)
+				return false;
+		} else if (!cdVenda.equals(other.cdVenda))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		return true;
+	}
+
 }
--- portal-corretor-servico/src/test/java/br/com/odontoprev/portal/corretor/test/controller/empresa/EmpresaControllerTest.java
+++ portal-corretor-servico/src/test/java/br/com/odontoprev/portal/corretor/test/controller/empresa/EmpresaControllerTest.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -27,6 +28,8 @@ import br.com.odontoprev.portal.corretor.dto.Empresa;
 import br.com.odontoprev.portal.corretor.dto.EmpresaArquivo;
 import br.com.odontoprev.portal.corretor.dto.EmpresaArquivoResponse;
 import br.com.odontoprev.portal.corretor.dto.EmpresaArquivoResponseItem;
+import br.com.odontoprev.portal.corretor.dto.EmpresaEmailAceite;
+import br.com.odontoprev.portal.corretor.dto.EmpresaResponse;
 import br.com.odontoprev.portal.corretor.service.EmpresaService;
 
 
@@ -107,7 +110,7 @@ public class EmpresaControllerTest {
 	}
 
 	@Test
-	public void testEmpresaArquivo200ComEmpresaNaoEncontrada() throws Exception {
+	public void testOk200EmpresaArquivoComEmpresaNaoEncontrada() throws Exception {
 
 	   	Long empresaPesquisada = 5448448L;
 	   	String mensagemEsperada = "Empresa nao encontrada.";
@@ -137,7 +140,7 @@ public class EmpresaControllerTest {
 	}
 
 	@Test
-	public void testEmpresaArquivoBadResquest400() throws Exception {
+	public void testBadResquest400EmpresaArquivo() throws Exception {
 
 		EmpresaArquivo cdEmpresa = new EmpresaArquivo();
 		cdEmpresa.setListCdEmpresa(new ArrayList<Long>());
@@ -152,5 +155,85 @@ public class EmpresaControllerTest {
 				.andExpect(status().isBadRequest())
 		;
 
+	}
+
+    //201809251930 - esert - COR-820 Criar POST /empresa-emailaceite
+	@Test
+	public void testOk200PostEmpresaEmailAceite() throws Exception {
+	
+		Long cdEmpresaGiven = 2566L;
+		Long cdVendaGiven = 4346L;
+		EmpresaEmailAceite empresaEmailAceite = new EmpresaEmailAceite();
+		empresaEmailAceite.setCdEmpresa(cdEmpresaGiven);
+		empresaEmailAceite.setCdVenda(cdVendaGiven);
+		String jsonRequest = new Gson().toJson(empresaEmailAceite);
+		
+		EmpresaResponse empresaResponse = new EmpresaResponse(HttpStatus.OK.value(), HttpStatus.OK.name());
+		//Mockando Service que busca no banco de dados
+		given(service.enviarEmpresaEmailAceite(empresaEmailAceite)).willReturn(empresaResponse);
+	
+		//Efetua a requisição na rota e espera um status code
+		mvc.perform(post("/empresa-emailaceite")
+				.content(jsonRequest)
+				.contentType(APPLICATION_JSON))
+				.andExpect(status().isOk())
+		;
+	
+	}
+	
+	//201809251945 - esert - COR-820 Criar POST /empresa-emailaceite
+	@Test
+	public void testBadRequest400PostEmpresaEmailAceite() throws Exception {
+		
+		Long cdEmpresaGiven = null;
+		Long cdVendaGiven = null;
+		EmpresaEmailAceite empresaEmailAceite = new EmpresaEmailAceite();
+		empresaEmailAceite.setCdEmpresa(cdEmpresaGiven);
+		empresaEmailAceite.setCdVenda(cdVendaGiven);
+		String jsonRequest = new Gson().toJson(empresaEmailAceite);
+		
+		EmpresaResponse empresaResponse = new EmpresaResponse(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.name());
+		
+		//Mockando Service que busca no banco de dados
+		given(service.enviarEmpresaEmailAceite(empresaEmailAceite)).willReturn(empresaResponse);
+		
+		//Efetua a requisição na rota e espera um status code
+		mvc.perform(post("/empresa-emailaceite")
+				.content(jsonRequest)
+				.contentType(APPLICATION_JSON))
+				.andExpect(status().isBadRequest())
+		;
+		
+	}
+	
+	//201809251956 - esert - COR-820 Criar POST /empresa-emailaceite
+	@Test
+	public void testNoContent204PostEmpresaEmailAceite() throws Exception {
+		
+		Long cdEmpresaGiven = 2566L;
+		Long cdVendaGiven = 4346L;
+		EmpresaEmailAceite empresaEmailAceiteGiven = new EmpresaEmailAceite();
+		empresaEmailAceiteGiven.setCdEmpresa(cdEmpresaGiven);
+		empresaEmailAceiteGiven.setCdVenda(cdVendaGiven);
+
+		EmpresaResponse empresaResponse = new EmpresaResponse(HttpStatus.OK.value(), HttpStatus.OK.name());
+
+		Long cdEmpresaReq = 2566L;
+		Long cdVendaReq = 2566L;
+		EmpresaEmailAceite empresaEmailAceiteReq = new EmpresaEmailAceite();
+		empresaEmailAceiteReq.setCdEmpresa(cdEmpresaReq);
+		empresaEmailAceiteReq.setCdVenda(cdVendaReq);
+		String jsonRequest = new Gson().toJson(empresaEmailAceiteReq);
+		
+		//Mockando Service que busca no banco de dados
+		given(service.enviarEmpresaEmailAceite(empresaEmailAceiteGiven)).willReturn(empresaResponse);
+		
+		//Efetua a requisição na rota e espera um status code
+		mvc.perform(post("/empresa-emailaceite")
+				.content(jsonRequest)
+				.contentType(APPLICATION_JSON))
+				.andExpect(status().isNoContent())
+		;
+		
 	}
 }
