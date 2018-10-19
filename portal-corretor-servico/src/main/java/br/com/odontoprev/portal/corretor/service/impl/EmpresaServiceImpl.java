9706d09a1e93b96515faa373e35309b93ae877eb
Yago Almeida Santos <yalmeida@vectoritcgroup.com>
2018-10-18 23:03:46 -0300

    Refatoracao: alteracao no convertToBase64

--- portal-corretor-servico/src/main/java/br/com/odontoprev/portal/corretor/service/impl/EmpresaServiceImpl.java
+++ portal-corretor-servico/src/main/java/br/com/odontoprev/portal/corretor/service/impl/EmpresaServiceImpl.java
@@ -859,33 +859,35 @@ public class EmpresaServiceImpl implements EmpresaService {
 		return fileUploadLoteDCMSResponse;
 	}
 
-	//201810051810 - esert - COR-861:Serviço - Receber / Retornar Planilha
-	private File convertBase64ToFile(String arquivoBase64, String nomeArquivo, String caminhoArquivo) {
-		log.info("convertBase64ToFile - ini");
-		File file = null;
-		byte[] byteArray = null;
-		if(caminhoArquivo==null || caminhoArquivo.trim().isEmpty()) { //201810052115 - esert - ferramenta para testes locais via postman
-			byteArray = Base64.getDecoder().decode(arquivoBase64);
-			log.info("pathXlsLoteDcms + nomeArquivo:[{}]", pathXlsLoteDcms + nomeArquivo);
-			try (OutputStream stream = new FileOutputStream(pathXlsLoteDcms + nomeArquivo)) { //201810111925 - esert - COR-861:Servico - Receber / Retornar Planilha - pathXlsLoteDcms 
-			    try {
-					stream.write(byteArray);
-				    file = new File(nomeArquivo);
-				} catch (IOException e) {
-					log.error("IOException", e);
-				}
-			} catch (FileNotFoundException e1) {
-				log.error("FileNotFoundException", e1);
-			} catch (IOException e1) {
-				log.error("IOException", e1);
-			}
-		} else {
-			String caminhoNomeArquivo = caminhoArquivo + "/" + nomeArquivo; //201810052115 - esert - ferramenta para testes locais via postman
-			file = new File(caminhoNomeArquivo); //201810052115 - esert - ferramenta para testes locais via postman
-		}
-		log.info("convertBase64ToFile - fim");
-		return file;
-	}
+    //201810051810 - esert - COR-861:Serviço - Receber / Retornar Planilha
+    private File convertBase64ToFile(String arquivoBase64, String nomeArquivo, String caminhoArquivo) {
+        log.info("convertBase64ToFile - ini");
+        File file = null;
+        byte[] byteArray = null;
+        if(caminhoArquivo==null || caminhoArquivo.trim().isEmpty()) { //201810052115 - esert - ferramenta para testes locais via postman
+            String caminhoNomeArquivo = pathXlsLoteDcms + "/" + nomeArquivo; //201810052115 - esert - ferramenta para testes locais via postman
+            log.info("caminhoNomeArquivo:[{}]", caminhoNomeArquivo);
+            file = new File(caminhoNomeArquivo);
+            byteArray = Base64.getDecoder().decode(arquivoBase64);
+            try (OutputStream stream = new FileOutputStream(file)) { //201810181928 //201810111925 - esert - COR-861:Servico - Receber / Retornar Planilha - pathXlsLoteDcms
+                try {
+                    stream.write(byteArray);
+                    //file = new File(nomeArquivo);
+                } catch (IOException e) {
+                    log.error("IOException", e);
+                }
+            } catch (FileNotFoundException e1) {
+                log.error("FileNotFoundException", e1);
+            } catch (IOException e1) {
+                log.error("IOException", e1);
+            }
+        } else {
+            String caminhoNomeArquivo = caminhoArquivo + "/" + nomeArquivo; //201810052115 - esert - ferramenta para testes locais via postman
+            file = new File(caminhoNomeArquivo); //201810052115 - esert - ferramenta para testes locais via postman
+        }
+        log.info("convertBase64ToFile - fim");
+        return file;
+    }
 
 	//201810051900 - esert - COR-861:Serviço - Receber / Retornar Planilha
 	@SuppressWarnings("deprecation")
@@ -1057,9 +1059,7 @@ public class EmpresaServiceImpl implements EmpresaService {
 		if(empresaDcmsLote==null) {
 			retorno += ",OBJETO NULO";
 		} else {
-			if(empresaDcmsLote.getCdVenda() != null 
-				&& 
-				empresaDcmsLote.getCdVenda().equals("CD_VENDA")) {
+			if(empresaDcmsLote.getCdVenda() != null) { // 201810182259 DEPLOY DE ULTIMA HORA
 				retorno = "HEADER"; //201810091122 - esert
 				return retorno; //201810091122 - esert
 			}
